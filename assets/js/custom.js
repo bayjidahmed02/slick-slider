f5ed76c7d1647d86ee38757e492ac5c0f2ca1337
Bayjid Ahmed <97084020+bayjidahmed02@users.noreply.github.com>
2022-07-19 15:49:01 +0600

    slick slider

---
+++ assets/js/custom.js
@@ -0,0 +1,11 @@
+$(".slider").slick({
+  infinity: true,
+  slidesToShow: 1,
+  autoplay: true,
+  dots: false,
+  arrows: true,
+  prevArrow:
+    '<span><i class="fa-solid fa-arrow-left-long left-arrow"></i></span>',
+  nextArrow:
+    '<span><i class="fa-solid fa-arrow-right-long right-arrow"></i></span>',
+});
